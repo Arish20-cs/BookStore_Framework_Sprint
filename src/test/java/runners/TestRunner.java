dc9d235d92d3cbbc5a3a2b8f7b60237bb7e0a8c4
Arish <arishxbma@gmail.com>
2026-04-21 08:28:54 +0530

    Commit_3 stepdefinitions

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,9 +4,19 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = "src/test/resources/features-package",
-	    glue = "stepDefinitions",
-	    tags = "@BookStoreWebAPI01TC_01 or @BookStoreWebAPI01TC_02 or @BookStoreWebAPI01TC_03 or @BookStoreWebAPI01TC_04 or @BookStoreWebAPI01TC_05 or @BookStoreWebAPI01TC_06 or @BookStoreWebAPI01TC_07 or @BookStoreWebAPI01TC_08 or @BookStoreWebAPI01TC_09 or @BookStoreWebAPI01TC_10 or @BookStoreWebAPI01TC_11 or @BookStoreWebAPI01TC_12 or @BookStoreWebAPI01TC_13 or @BookStoreWebAPI01TC_14 or @BookStoreWebAPI01TC_15 or @BookStoreWebAPI01TC_16 or @BookStoreWebAPI01TC_17"
-	)
+    features = "src/test/resources/features-package",
+    glue = "stepDefinitions",
+    
+    
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "junit:target/cucumber.xml"
+    },
+
+    monochrome = true,
+    publish = true
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
